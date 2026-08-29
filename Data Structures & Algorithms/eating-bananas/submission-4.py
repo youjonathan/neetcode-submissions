7fc6e25eb40e4a393e69e404fa373484202ddba8
jonathan <60077409+youjonathan@users.noreply.github.com>
2026-08-28 21:19:05 -0400

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        # binary search to find k
+        max_k = max(piles)
+        
+        # if k is the max in piles, the max h is len(piles)
+        # if it's less, we find the ceiling of the division per element
+        l = 0
+        r = max_k
+        k = r // 2
+
+        while r - l > 1: # idk
+            curr_h = 0
+            for num in piles:
+                curr_h += math.ceil(num / k)
+            
+            if curr_h <= h:
+                r = k
+            else:
+                l = k
+
+            # this is the binary search
+            k = l + (r - l) // 2
+        
+        return r
