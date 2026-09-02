1bf3d0c3fef2588e346744157cafc80b59124a53
jonathan <60077409+youjonathan@users.noreply.github.com>
2026-09-01 20:36:43 -0400

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        # brute force would prob be O(n^2) by just looping through
+        # checking if a node is = to any other node's next
+        # wait no but when would we know when to stop the while loop?
+        
+        # two pointers loop
+        # so the idea is that if you have one going one at a time
+        # and the other going twice the speed
+        # if there is a cycle they will eventually intersect
+        # but when will it end if they don't intersect?
+        # guessing if they get to a null next
+
+        first = head
+        second = head
+
+        while first and second:
+            first = first.next
+            second = second.next.next
+            if first == second:
+                return True
+
+        return False
