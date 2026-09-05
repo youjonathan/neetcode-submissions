9c82e425d4ae122d4c94535b2083dfe4670f7711
jonathan <60077409+youjonathan@users.noreply.github.com>
2026-09-04 20:55:28 -0400

    Add: remove-node-from-end-of-linked-list - submission-4

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-4.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        # brute force is to keep a count to count total num of nodes
+        # then once you reach the end, go to count - n node and edit it
+
+        # how would you increase the efficiency?
+        # one way is keeping an array of all nodes?
+        # num nodes is constrained to 30 so wouldn't be too costly
+
+        arr = []
+        curr = head
+        while curr:
+            arr.append(curr)
+            curr = curr.next
+
+        index = len(arr) - n
+        if index == 0:
+            if len(arr) == 0:
+                return None
+            else:
+                return head.next
+        
+        prev = arr[index - 1]
+        if index + 1 > len(arr):
+            prev.next = None
+        else:
+            prev.next = arr[index + 1]
+
+        return head
+        
+        
