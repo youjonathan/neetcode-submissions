cc5269c8934528fe9c5c0f891da732799a440328
jonathan <60077409+youjonathan@users.noreply.github.com>
2026-09-16 19:59:21 -0400

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        if amount == 0:
+            return 0
+
+        memo = {}
+
+        def findCombinations(remaining):
+            if remaining < 0:
+                return math.inf
+            elif remaining == 0:
+                return 0
+            least = math.inf
+            for coin in coins:
+                least = min(1 + findCombinations(remaining - coin), least)
+            memo[remaining] = least
+            return memo[remaining]
+
+        lowest = findCombinations(amount)
+        return -1 if lowest == math.inf else int(lowest)
