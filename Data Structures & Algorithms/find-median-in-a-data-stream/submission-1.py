72be5e13cd3e3796a05044f92ee9c0d6cf014029
jonathan <60077409+youjonathan@users.noreply.github.com>
2026-09-18 16:25:20 -0400

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,14 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.arr = []
+
+    def addNum(self, num: int) -> None:
+        self.arr.append(num)
+        return
+
+    def findMedian(self) -> float:
+        self.arr.sort()
+        if len(self.arr) % 2 == 1:
+            return self.arr[len(self.arr) // 2]
+        return (self.arr[len(self.arr) // 2] + self.arr[len(self.arr) // 2 - 1]) / 2
